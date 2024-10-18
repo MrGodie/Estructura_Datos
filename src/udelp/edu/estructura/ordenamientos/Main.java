8d6bda5800615ddebc7341ad79151287c7945f35
LEN0\dhalf <dhalfaro560@gmail.com>
2024-10-18 07:54:56 -0600

    ORDENAMIENTOS

--- src/udelp/edu/estructura/ordenamientos/Main.java
+++ src/udelp/edu/estructura/ordenamientos/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	Scanner leer = new Scanner(System.in);
 	DoubleLinkedList lista = new DoubleLinkedList();	
 	String elegir;
+	int buscar;
 	
 	for(int i = 0; i < 20; i++) {
 		lista.add(i, aleatorio.nextInt(0, 101));
@@ -23,13 +24,21 @@ public class Main {
 	switch (elegir) {
 	case "1":
 		Funciones.ordenamientoQuickSort(lista, (lista.size()));
-		System.out.println("Lista ordenada: ");
+		System.out.println("Lista ordenada");
+		System.out.println("Que numero quiere buscar?");
+		buscar = leer.nextInt();
+		System.out.println("Indice: "+Funciones.busquedaBinaria(lista, lista.size(), buscar));
 		System.out.println(lista.toString());
+		
 		break;
 	case "2":
 		Funciones.ordenamientoShellSort(lista, (lista.size()));
-		System.out.println("Lista ordenada: ");
+		System.out.println("Lista ordenada");
+		System.out.println("Que numero quiere buscar?");
+		buscar = leer.nextInt();
+		System.out.println("Indice: "+Funciones.busquedaBinaria(lista, lista.size(), buscar));
 		System.out.println(lista.toString());
+		
 		break;
 	default:
 		System.out.println("Seleccion invalida");
